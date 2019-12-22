2bec64cf735234b379e2d8a45bfea64fbb0a461c
Derek Miller <dmiller@newrelic.com>
2019-12-21 16:28:25 -0800

    start using types and return json

--- main.go
+++ main.go
@@ -4,21 +4,41 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) error {
-	usernames := []string{"krudler", "krudlers"}
-	url := "https://twitter.com/"
-	for _, username := range usernames {
-		resp, err := http.Get(url + username)
-		if err != nil {
-			return fmt.Errorf("could not get url: %v", err)
-		}
-		fmt.Printf("%v", resp.StatusCode)
+const twitterURL = "https://twitter.com/"
+
+type DesiredUsernames struct {
+	Usernames []string `json:"usernames"`
+}
+
+type AvailableUsernames struct {
+	Usernames []string `json:"usernames"`
+}
+
+func handler() (AvailableUsernames, error) {
+	desiredUsernames, err := getDesiredUsernames()
+	if err != nil {
+		return AvailableUsernames{}, fmt.Errorf("could not get desired usernames: %v", err)
 	}
-	return nil
+
+	var availableUsernames []string
+	for _, username := range desiredUsernames.Usernames {
+		resp, err := http.Get(twitterURL + username)
+		if err != nil {
+			return AvailableUsernames{Usernames: availableUsernames}, fmt.Errorf("could not get url: %v", err)
+		}
+		if resp.StatusCode == http.StatusNotFound {
+			availableUsernames = append(availableUsernames, username)
+		}
+	}
+
+	return AvailableUsernames{Usernames: availableUsernames}, nil
+}
+
+func getDesiredUsernames() (DesiredUsernames, error) {
+	return DesiredUsernames{Usernames: []string{"krudler", "crudler", "derek", "hypertrace"}}, nil
 }
 
 func main() {
